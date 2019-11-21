f27e3d3fa26ea3ead9cd5c6f84892577f1aa145f
Aaron Zhang <kingmanzhang@gmail.com>
2019-11-21 17:31:46 -0500

    add more support

--- obonet/ontology.py
+++ obonet/ontology.py
@@ -4,12 +4,29 @@ import copy
 
 
 class Ontology:
+    """
+    Class to represent an ontology. The underlying data structure is a
+    networkx MultiDiGraph. The class simplies some commonly used functions
+    in a similar way to the Java phenol library.
+    phenol: https://github.com/monarch-initiative/phenol/blob/master/phenol-core/src/main/java/org/monarchinitiative/phenol/ontology/algo/OntologyAlgorithm.java
+    """
     def __init__(self, path):
         """
         Initialize an ontology class by providing the path.
         :param path:
         """
         self.graph = read_obo(path)
+        self.root_id = self._find_root_id()
+
+    def _find_root_id(self):
+        """
+        Find the root id
+        :return: root id
+        """
+        for node in self.graph.nodes:
+            if len(self.graph[node]) == 0:
+                return node
+        raise RuntimeError("root term not found")
 
     def nx_graph(self, deepcopy=False):
         """
@@ -26,39 +43,62 @@ class Ontology:
         else:
             return self.graph
 
-    def ancestors(self, term_id):
+    def get_root_id(self):
+        """
+        Returns the root id
+        :return: root id
+        """
+        return self.root_id
+
+    def ancestors(self, term_id, include_self=False):
         """
         Returns all the ancestor ontology terms (more generic terms). Note
         "ancestor" here is opposite to networkx "ancestor".
         :param term_id: query term
+        :param include_self: whether to include the term itself
         :return: all ancestors
         """
-        return nx.descendants(self.graph, term_id)
+        ancestor_set = nx.descendants(self.graph, term_id)
+        if include_self:
+            ancestor_set.add(term_id)
+        return ancestor_set
 
-    def descendants(self, term_id):
+    def descendants(self, term_id, include_self=False):
         """
         Returns all the descendant ontology terms (more specific terms). Note
         "descendant" here is opposite to networkx "descendant".
         :param term_id: query term
+        :param include_self: whether to include the term itself
         :return: all descendants
         """
-        return nx.ancestors(self.graph, term_id)
+        descendant_set = nx.ancestors(self.graph, term_id)
+        if include_self:
+            descendant_set.add(term_id)
+        return descendant_set
 
-    def parents(self, term_id):
+    def parents(self, term_id, include_self=False):
         """
         Returns the immediate parents of a term
         :param term_id: query term
+        :param include_self: whether to include the term itself
         :return: parents
         """
-        return frozenset(self.graph[term_id].keys())
+        parent_set = set(self.graph[term_id].keys())
+        if include_self:
+            parent_set.add(term_id)
+        return parent_set
 
-    def children(self, term_id):
+    def children(self, term_id, include_self=False):
         """
         Returns the immediate children of a term
         :param term_id: query term
         :return: children terms
+        :param include_self: whether to include the term itself
         """
-        return frozenset(self.graph.predecessors(term_id))
+        child_set = set(self.graph.predecessors(term_id))
+        if include_self:
+            child_set.add(term_id)
+        return child_set
 
     def terms(self):
         """
@@ -75,3 +115,51 @@ class Ontology:
         """
         id_2_label = copy.deepcopy(nx.get_node_attributes(self.graph, 'name'))
         return id_2_label
+
+    def exists_path(self, src_id, dest_id):
+        """
+        Checks whether there is a path from the source node to the
+        destination node on the ontology tree (from most generic to most
+        specific)
+        :param src_id: source term id
+        :param dest_id: destination term id
+        :return: true iff src_id is an ancestor of dest_id. It throws a
+        runtime error if two terms are identical.
+        """
+        if src_id == dest_id:
+            raise RuntimeError("cannot decide whether there is path to itself")
+        return src_id in self.ancestors(dest_id, include_self=False)
+
+    def terms_are_siblings(self, t1, t2):
+        """
+        Checks whether two terms are the siblings.
+        :param t1:
+        :param t2:
+        :return:
+        """
+        if t1 == t2:
+            raise RuntimeError("cannot decide a term is its own sibling")
+        parent1 = self.parents(t1, include_self=False)
+        parent2 = self.parents(t2, include_self=False)
+        return len(parent1.intersection(parent2)) > 0
+
+    def terms_are_related(self, t1, t2):
+        """
+        Checks whether two terms are related.
+        :param t1: term id 1
+        :param t2: term id 2
+        :return: true iff t1 and t2 have a common ancestor that is not the
+        root of the ontology. It throws a runtime error if two terms are
+        identical.
+        """
+        # TODO: waiting for resource to be fully tested
+        raise NotImplementedError()
+        # if t1 == t2:
+        #     raise RuntimeError("cannot decide a terms is related to itself")
+        # aces1 = self.ancestors(t1, include_self=False)
+        # aces2 = self.ancestors(t2, include_self=False)
+        # inst = aces1.interset(aces2)
+        # for t in inst:
+        #     if t != self.root_id:
+        #         return True
+        # return False
--- tests/test_ontology.py
+++ tests/test_ontology.py
@@ -14,22 +14,34 @@ class TestOntologyClass:
     def test_constructor(self):
         assert(self.onto is not None)
 
+    def test_root_id(self):
+        assert(self.onto.get_root_id() == self.root)
+
     def test_ancestors(self):
 
         assert(len(self.onto.ancestors(self.root)) == 0)
+        assert(len(self.onto.ancestors(self.root, include_self=True)) == 1)
 
         term_id = 'TAXRANK:0000003'
         assert(self.onto.ancestors(term_id) == {'TAXRANK:0000000'})
+        assert (self.onto.ancestors(term_id, include_self=True) == {
+            'TAXRANK:0000000', term_id})
 
     def test_descendants(self):
         assert(len(self.onto.descendants(self.root)) > 0)
+        assert(len(self.onto.descendants(self.root, include_self=True)) -
+               len(self.onto.descendants(self.root, include_self=False)) == 1)
 
     def test_parents(self):
         term_id = 'TAXRANK:0000004'
         assert(self.onto.parents(term_id) == {'TAXRANK:0000000'})
+        assert (self.onto.parents(term_id, include_self=True) == {
+            'TAXRANK:0000000', term_id})
 
     def test_children(self):
         assert(len(self.onto.children(self.root)) > 0)
+        assert (len(self.onto.children(self.root, include_self=True)) -
+                len(self.onto.children(self.root, include_self=False)) == 1)
 
     def test_term_id2label_map(self):
         assert(len(self.onto.term_id_2_label_map()) > 0)
@@ -43,3 +55,27 @@ class TestOntologyClass:
         shallowcopy.remove_node('TAXRANK:0000004')
         assert(len(shallowcopy.nodes) == len(self.onto.graph.nodes))
         assert(len(deepcopy.nodes) != len(self.onto.graph.nodes))
+
+    def test_exists_path(self):
+        t1 = 'TAXRANK:0000000'
+        t2 = 'TAXRANK:0000004'
+        t3 = 'TAXRANK:0000005'
+        assert(self.onto.exists_path(t1, t2))
+        assert(self.onto.exists_path(t1, t3))
+        assert(not self.onto.exists_path(t2, t1))
+        assert(not self.onto.exists_path(t2, t3))
+
+    def test_terms_are_siblings(self):
+        t1 = 'TAXRANK:0000000'
+        t2 = 'TAXRANK:0000004'
+        t3 = 'TAXRANK:0000005'
+        assert (self.onto.terms_are_siblings(t2, t3))
+        assert (not self.onto.terms_are_siblings(t1, t2))
+        assert (not self.onto.terms_are_siblings(t1, t3))
+
+    # def test_terms_are_related(self):
+    #     t1 = 'TAXRANK:0000000'
+    #     t2 = 'TAXRANK:0000004'
+    #     t3 = 'TAXRANK:0000005'
+    #     assert (self.onto.terms_are_related(t2, t3))
+    #     assert (not self.onto.terms_are_related(t1, t2))
